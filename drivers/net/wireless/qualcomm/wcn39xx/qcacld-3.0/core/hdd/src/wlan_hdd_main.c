7ded9f990970b9bd3cd9965fd641f17abc8581c7
schr-0dinger <kmkailasnath@gmail.com>
2023-02-01 19:31:28 +0530

    Revert "qcacld-3.0: Fix clang warnings sprintf will always overflow; destination buffer has size 1, but format string expands to atleast 3"
    This reverts commit a404f55d575e343b13725aa6298d03cc79a5a1e8.

--- drivers/net/wireless/qualcomm/wcn39xx/qcacld-3.0/core/hdd/src/wlan_hdd_main.c
+++ drivers/net/wireless/qualcomm/wcn39xx/qcacld-3.0/core/hdd/src/wlan_hdd_main.c
@@ -14249,7 +14249,7 @@ static ssize_t show_wificableinfo(struct kobject *kobj,
 {
 	struct device_node *np;
 	int wifi_cable = 0;
-	char antbuffer[3] = {0};
+	char antbuffer[1] = {0};
 
 	np = of_find_compatible_node(NULL, NULL, "samsung,rome_cable");
 
