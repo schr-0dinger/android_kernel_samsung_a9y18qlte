1febd128d773292c15fe37a44a84d921a5c3bbae
schr-0dinger <kmkailasnath@gmail.com>
2026-03-30 22:35:22 +0530

    Modify build_kernel.sh

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,13 +1,26 @@
-
 #!/bin/bash
 
 export PATH=$(pwd)/../PLATFORM/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin:$PATH
-# export SEC_BUILD_OPTION_HW_REVISION=02
 
-mkdir out
+mkdir -p out
 
-make -C $(pwd) O=$(pwd)/out ARCH=arm64 CROSS_COMPILE=aarch64-linux-android- KCFLAGS=-mno-android VARIANT_DEFCONFIG=sdm660_sec_a9y18qlte_eur_open_defconfig sdm660_sec_defconfig SELINUX_DEFCONFIG=selinux_defconfig
-make -j64 -C $(pwd) O=$(pwd)/out ARCH=arm64 CROSS_COMPILE=aarch64-linux-android- KCFLAGS=-mno-android
+make -C $(pwd) O=$(pwd)/out ARCH=arm64 CROSS_COMPILE=aarch64-linux-android- \
+  KCFLAGS=-mno-android \
+  VARIANT_DEFCONFIG=sdm660_sec_a9y18qlte_eur_open_defconfig \
+  sdm660_sec_defconfig \
+  SELINUX_DEFCONFIG=selinux_defconfig
 
-cp out/arch/arm64/boot/Image $(pwd)/arch/arm64/boot/Image
+scripts/config --file $(pwd)/out/.config \
+  --disable MODULE_SIG \
+  --disable MODULE_SIG_FORCE \
+  --disable MODULE_SIG_ALL \
+  --disable SYSTEM_TRUSTED_KEYRING \
+  --set-str SYSTEM_TRUSTED_KEYS ""
 
+make -C $(pwd) O=$(pwd)/out ARCH=arm64 CROSS_COMPILE=aarch64-linux-android- olddefconfig
+
+# build Image.gz-dtb
+make -j4 -C $(pwd) O=$(pwd)/out ARCH=arm64 CROSS_COMPILE=aarch64-linux-android- \
+  KCFLAGS=-mno-android Image.gz-dtb
+
+cp out/arch/arm64/boot/Image.gz-dtb $(pwd)/arch/arm64/boot/Image.gz-dtb
